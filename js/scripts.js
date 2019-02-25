7bd0767af010b9c9be82e723f171354fadf21096
badrubadri92@gmail.com <baddrubadri92@gmail.com>
2019-02-25 09:27:54 +0300

    modified js

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,9 @@ $(document).ready(function() {
     event.preventDefault();
     var choices1 = parseInt($("input:checkbox[name=choice1]:checked").val());
     var choices2 = parseInt($("input:checkbox[name=choice2]:checked").val());
-    var score = (choices1 + choices2);
+    var choices3 = parseInt($("input:checkbox[name=choice3]:checked").val());
+    var choices4 = parseInt($("input:checkbox[name=choice4]:checked").val());
+    var score = (choices1 + choices2 + choices3 + choices4);
     $("#finalScore").text(score);
   });
 });
