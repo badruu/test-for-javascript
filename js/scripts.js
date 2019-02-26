96e33b408f1291cc9d90cd3ec0a1b4b63c261867
badrubadri92@gmail.com <baddrubadri92@gmail.com>
2019-02-26 10:04:55 +0300

    add function for submit buttons

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,26 @@
-inputIds=[]
-values=[]
 $(document).ready(function() {
-  $("#submit").click(function() {
-    //event.preventDefault();
+  $("#details").submit(function(event) {
+    event.preventDefault();
+    var firstNameInput = $("#firstName").val();
+    var middleInitialInput = $("input#middleInitial").val();
+    var lastNameInput = $("input#lastName").val();
+
+    $(".1stName").text(firstNameInput);
+    $(".middleInit").text(middleInitialInput);
+    $(".2ndName").text(lastNameInput);
+  });
+  $("#tests").submit(function(event) {
+    event.preventDefault();
     var choices1 = parseInt($("input:checkbox[name=choice1]:checked").val());
     var choices2 = parseInt($("input:checkbox[name=choice2]:checked").val());
     var choices3 = parseInt($("input:checkbox[name=choice3]:checked").val());
     var choices4 = parseInt($("input:checkbox[name=choice4]:checked").val());
     var score = (choices1 + choices2 + choices3 + choices4);
-
-
-//    $('#tests').hide();
-    $("#finalScore").text('Your score is'+score);
-  });
+    $(".finalScore").text(score);
+  })
   $("#button").click(function() {
     location.replace('#exam')
     $('.general-details').hide();
     $("#exam").show();
-  });
-
-
-  $('#details').click(function(){
-    $("input").each(function(){
-      var inputId=$(this).attr('id');
-      inputIds.push(inputId)
-    })
-    getValue();
-  })
-
+ });
 });
-
-function getValue(){
-  alert(inputIds)
-  for(i=0;i<inputIds.length;i+=1){
-    var value=$(inputIds[i]).val()
-
-    values.push(value)
-
-  }
-  alert(values)
-}
